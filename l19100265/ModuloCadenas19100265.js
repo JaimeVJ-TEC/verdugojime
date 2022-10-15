9e6c22d71391561812fa7adba84c082c9975c03f
JaimeVJ-TEC <l19100265@nlaredo.tecnm.mx>
2022-10-14 20:33:10 -0500

    Paquete npm publicado

--- l19100265/ModuloCadenas19100265.js
+++ l19100265/ModuloCadenas19100265.js
@@ -10,6 +10,11 @@ function cadenaLongitud(cadena) {
     return cadena.length;
 }
 
+function cadenaCantidadCaracter(cadena,caracter) {
+    return cadena.split(caracter).length -1;
+}
+
 exports.cadenaMayusculas = cadenaMayusculas;
 exports.cadenaSinEspacios = cadenaSinEspacios;
 exports.cadenaLongitud = cadenaLongitud;
+exports.cadenaCantidadCaracter = cadenaCantidadCaracter;
